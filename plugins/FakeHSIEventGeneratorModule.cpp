9cbf28c1f070ff6c1d975f24b58cedb870553567
MRiganSUSX <M.Rigan@sussex.ac.uk>
2025-08-13 12:09:11 +0200

    trying 1ns

--- plugins/FakeHSIEventGeneratorModule.cpp
+++ plugins/FakeHSIEventGeneratorModule.cpp
@@ -245,7 +245,7 @@ FakeHSIEventGeneratorModule::do_hsi_work(std::atomic<bool>& running_flag)
 {
   TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Entering generate_hsievents() method";
 
-  std::this_thread::sleep_for(1us);
+  std::this_thread::sleep_for(1ns);
 
   // Wait for there to be a valid timestsamp estimate before we start
   // TODO put in tome sort of timeout? Stoyan Trilov stoyan.trilov@cern.ch
